dcab09a84a0d49ec909850f5f3cfba9c92bbc20a
kontashi35 <khakontas@gmail.com>
2020-01-11 18:24:13 +0530

    id

--- src/main/java/com/example/blog/BlogApi/domain/Blog.java
+++ src/main/java/com/example/blog/BlogApi/domain/Blog.java
@@ -1,14 +1,12 @@
 package com.example.blog.BlogApi.domain;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "BLOG")
 public class Blog {
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @javax.persistence.Id
     @Column(name = "ID")
     private Long Id;
